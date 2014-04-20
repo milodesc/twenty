c27995abb66d48cb98753114e2a1449845dfeb39
Patrick Jensen <patrick.c.jensen@gmail.com>
2014-04-20 19:03:45 -0400

    initial efforts. very broken.

---
+++ templates/html--front.tpl.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
+<!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
+<!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
+<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
+
+<head>
+    <?php print $head; ?>
+    <title><?php print $head_title; ?></title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+
+    <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+    <?php print $styles; ?>
+    <?php print $scripts; ?>
+    <noscript>
+        <link rel="stylesheet" href="css/skel.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/style-noscript.css" />
+    </noscript>
+    <!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+    <!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<?php if ($skip_link_text && $skip_link_anchor): ?>
+    <p id="skip-link">
+        <a href="#<?php print $skip_link_anchor; ?>" class="element-invisible element-focusable"><?php print $skip_link_text; ?></a>
+    </p>
+<?php endif; ?>
+<?php print $page_top; ?>
+<?php print $page; ?>
+<?php print $page_bottom; ?>
+</body>
+</html>
