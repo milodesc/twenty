9e99c4c06aacd4189b2e7a3fc37c513eb36f895f
Patrick Jensen <patrick.c.jensen@gmail.com>
2014-04-21 07:46:49 -0400

    minor adjustments to stay a bit more true to HTML 5 Up's implementation

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
 <!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
 <!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
@@ -6,10 +6,8 @@
 <!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
 
 <head>
-    <?php print $head; ?>
     <title><?php print $head_title; ?></title>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-
+    <?php print $head; ?>
 
     <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
     <?php print $styles; ?>
