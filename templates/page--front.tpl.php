c1ba0c9b3756d1f063ac586819404627880f7ba4
Patrick Jensen <patrick.c.jensen@gmail.com>
2014-04-21 08:33:03 -0400

    make the site header true to HTML5 Up's template

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -60,22 +60,10 @@
 
     <header class="header" id="header" role="banner">
 
-        <?php if ($logo): ?>
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-        <?php endif; ?>
-
         <?php if ($site_name || $site_slogan): ?>
-            <div class="header__name-and-slogan" id="name-and-slogan">
-                <?php if ($site_name): ?>
-                    <h1 class="header__site-name" id="site-name">
-                        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-                    </h1>
-                <?php endif; ?>
-
-                <?php if ($site_slogan): ?>
-                    <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-                <?php endif; ?>
-            </div>
+            <h1 id="logo">
+                <a href="<?php print $front_page; ?>"><?php print $site_name; ?> <span><?php print $site_slogan; ?></span></a>
+            </h1>
         <?php endif; ?>
 
         <?php if ($secondary_menu): ?>
