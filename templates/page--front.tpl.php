b986a549baf3a6cadd94fc4ebd531576c09343fb
Patrick Jensen <patrick.c.jensen@gmail.com>
2014-04-24 17:55:39 -0400

    added footer stuff

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -205,3 +205,34 @@
         </section>
 
     </article>
+
+    <!-- CTA -->
+    <section id="cta">
+
+        <header>
+            <h2>Ready to do <strong>something</strong>?</h2>
+            <p>Proin a ullamcorper elit, et sagittis turpis integer ut fermentum.</p>
+        </header>
+        <footer>
+            <ul class="buttons">
+                <li><a href="#" class="button special">Take My Money</a></li>
+                <li><a href="#" class="button">LOL Wut</a></li>
+            </ul>
+        </footer>
+
+    </section>
+
+    <!-- Footer -->
+    <footer id="footer">
+
+        <ul class="icons">
+            <li><a href="#" class="icon circle fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon circle fa-google-plus"><span class="label">Google+</span></a></li>
+            <li><a href="#" class="icon circle fa-github"><span class="label">Github</span></a></li>
+            <li><a href="#" class="icon circle fa-dribbble"><span class="label">Dribbble</span></a></li>
+        </ul>
+
+        <span class="copyright">&copy; Untitled. All rights reserved. Design: <a href="http://html5up.net">HTML5 UP</a>.</span>
+
+    </footer>
