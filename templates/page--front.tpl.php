4f8713c2ed5cf59f77365e46ac6bffb086f0e047
Patrick Jensen <patrick.c.jensen@gmail.com>
2014-04-24 08:41:44 -0400

    spring cleaning: remove a bunch of unneeded php on the front page template

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -36,6 +36,9 @@
 
     <!-- Banner -->
     <section id="banner">
+
+        <?php print $messages; ?>
+
         <!--
             ".inner" is set up as an inline-block so it automatically expands
             in both directions to fit whatever's inside it. This means it won't
@@ -202,70 +205,3 @@
         </section>
 
     </article>
-
-    <div id="main">
-
-        <div id="content" class="column" role="main">
-            <?php print render($page['highlighted']); ?>
-            <?php print $breadcrumb; ?>
-            <a id="main-content"></a>
-            <?php print render($title_prefix); ?>
-            <?php if ($title): ?>
-                <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-            <?php endif; ?>
-            <?php print render($title_suffix); ?>
-            <?php print $messages; ?>
-            <?php print render($tabs); ?>
-            <?php print render($page['help']); ?>
-            <?php if ($action_links): ?>
-                <ul class="action-links"><?php print render($action_links); ?></ul>
-            <?php endif; ?>
-            <?php print render($page['content']); ?>
-            <?php print $feed_icons; ?>
-        </div>
-
-        <div id="navigation">
-
-            <?php if ($main_menu): ?>
-                <nav id="main-menu" role="navigation" tabindex="-1">
-                    <?php
-                    // This code snippet is hard to modify. We recommend turning off the
-                    // "Main menu" on your sub-theme's settings form, deleting this PHP
-                    // code block, and, instead, using the "Menu block" module.
-                    // @see https://drupal.org/project/menu_block
-                    print theme('links__system_main_menu', array(
-                        'links' => $main_menu,
-                        'attributes' => array(
-                            'class' => array('links', 'inline', 'clearfix'),
-                        ),
-                        'heading' => array(
-                            'text' => t('Main menu'),
-                            'level' => 'h2',
-                            'class' => array('element-invisible'),
-                        ),
-                    )); ?>
-                </nav>
-            <?php endif; ?>
-
-            <?php print render($page['navigation']); ?>
-
-        </div>
-
-        <?php
-        // Render the sidebars to see if there's anything in them.
-        $sidebar_first  = render($page['sidebar_first']);
-        $sidebar_second = render($page['sidebar_second']);
-        ?>
-
-        <?php if ($sidebar_first || $sidebar_second): ?>
-            <aside class="sidebars">
-                <?php print $sidebar_first; ?>
-                <?php print $sidebar_second; ?>
-            </aside>
-        <?php endif; ?>
-
-    </div>
-
-    <?php print render($page['footer']); ?>
-
-<?php print render($page['bottom']); ?>
