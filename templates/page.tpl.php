758a35c196c22593bd7debc2035c9ee888dc341e
Patrick Jensen <patrick.c.jensen@gmail.com>
2014-05-01 08:46:34 -0400

    getting the structure of the left sidebar tpl set up

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -33,58 +33,74 @@
 
     </header>
 
-    <!-- Banner -->
-    <section id="banner">
-
-        <?php print $messages; ?>
-
-        <!--
-            ".inner" is set up as an inline-block so it automatically expands
-            in both directions to fit whatever's inside it. This means it won't
-            automatically wrap lines, so be sure to use line breaks where
-            appropriate (<br />).
-        -->
-        <div class="inner">
-            <header>
-                <h2><?php print $site_name; ?></h2>
-            </header>
-            <p><?php print $site_slogan; ?></p>
-            <footer>
-                <ul class="buttons vertical">
-                    <li><a href="#main" class="button fit scrolly">Tell Me More</a></li>
-                </ul>
-            </footer>
-        </div>
-    </section>
-
     <!-- Main -->
     <article id="main">
 
-        <?php if($page['front_stack_1']): ?>
-            <!--start front_stack_1 -->
-            <header class="special container">
-                <span class="icon fa-bar-chart-o"></span>
-                <?php print render($page['front_stack_1']); ?>
-            </header>
-            <!-- end front_stack_1 -->
-        <?php endif; ?>
+        <header class="special container">
+            <span class="icon fa-laptop"></span>
+            <?php print render($title_prefix); ?>
+            <h2>
+                <?php if ($title):
+                   print $title;
+                endif; ?>
+            </h2>
+            <?php print render($title_suffix); ?>
+            <p><?php //print render(node_view($node, 'teaser')); ?></p>
+        </header>
 
-        <?php if($page['front_color_highlighted_2']): ?>
-            <!--start front_color_highlighted_2 -->
-            <section class="wrapper style2 container special-alt">
-                <div class="row half">
-                    <div class="8u">
-                        <?php print render($page['front_color_highlighted_2']); ?>
+        <!-- One -->
+        <section class="wrapper style4 container">
+
+            <div class="row oneandhalf">
+                <div class="4u">
+
+                    <!-- Sidebar -->
+                    <div class="sidebar">
+                        <section>
+                            <header>
+                                <h3>Magna Feugiat</h3>
+                            </header>
+                            <p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit..</p>
+                            <footer>
+                                <ul class="buttons">
+                                    <li><a href="#" class="button small">Learn More</a></li>
+                                </ul>
+                            </footer>
+                        </section>
+
+                        <section>
+                            <a href="#" class="image feature"><img src="images/pic03.jpg" alt="" /></a>
+                            <header>
+                                <h3>Amet Lorem Tempus</h3>
+                            </header>
+                            <p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
+                            <footer>
+                                <ul class="buttons">
+                                    <li><a href="#" class="button small">Learn More</a></li>
+                                </ul>
+                            </footer>
+                        </section>
                     </div>
-                    <?php if($page['front_color_highlighted_icon_list_2']): ?>
-                        <div class="4u skel-cell-important">
-                            <?php print render($page['front_color_highlighted_icon_list_2']); ?>
-                        </div>
-                    <?php endif; ?>
+
                 </div>
-            </section>
-            <!-- end front_color_highlighted_2-->
-        <?php endif; ?>
+                <div class="8u skel-cell-important">
+
+                    <!-- Content -->
+                    <div class="content">
+                        <section>
+                            <a href="#" class="image feature"><img src="images/pic02.jpg" alt="" /></a>
+                            <header>
+                                <h3>Dolore Amet Consequat</h3>
+                            </header>
+                            <p>Aliquam massa urna, imperdiet sit amet mi non, bibendum euismod est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam quam, mollis at consectetur non, malesuada quis augue. Morbi tincidunt pretium interdum. Morbi mattis elementum orci, nec dictum massa. Morbi eu faucibus massa. Aliquam massa urna, imperdiet sit amet mi non, bibendum euismod est. Curabitur mi justo, tincidunt vel eros ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam.</p>
+                            <p>Vestibulum diam quam, mollis at consectetur non, malesuada quis augue. Morbi tincidunt pretium interdum. Morbi mattis elementum orci, nec dictum porta cursus justo. Quisque ultricies lorem in ligula condimentum, et egestas turpis sagittis. Cras ac nunc urna. Nullam eget lobortis purus. Phasellus vitae tortor non est placerat tristique. Sed id sem et massa ornare pellentesque. Maecenas pharetra porta accumsan. </p>
+                            <p>In vestibulum massa quis arcu lobortis tempus. Nam pretium arcu in odio vulputate luctus. Suspendisse euismod lorem eget lacinia fringilla. Sed sed felis justo. Nunc sodales elit in laoreet aliquam. Nam gravida, nisl sit amet iaculis porttitor, risus nisi rutrum metus, non hendrerit ipsum arcu tristique est.</p>
+                        </section>
+                    </div>
+
+                </div>
+            </div>
+        </section>
 
         <!-- Two -->
         <?php if ($page['triptych_first'] || $page['triptych_middle'] || $page['triptych_last']): ?>
@@ -118,88 +134,7 @@
             </section>
         <?php endif; ?>
 
-
-        <!-- Three -->
-        <section class="wrapper style3 container special">
-
-            <header class="major">
-                <h2>Next look at this <strong>cool stuff</strong></h2>
-            </header>
-
-            <div class="row">
-                <div class="6u">
-
-                    <section>
-                        <a href="#" class="image feature"><img src="<?php print drupal_get_path("theme", "twenty"); ?>/images/pic01.jpg" alt="" /></a>
-                        <header>
-                            <h3>A Really Fast Train</h3>
-                        </header>
-                        <p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
-                    </section>
-
-                </div>
-                <div class="6u">
-
-                    <section>
-                        <a href="#" class="image feature"><img src="<?php print drupal_get_path("theme", "twenty"); ?>/images/pic02.jpg" alt="" /></a>
-                        <header>
-                            <h3>An Airport Terminal</h3>
-                        </header>
-                        <p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
-                    </section>
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="6u">
-
-                    <section>
-                        <a href="#" class="image feature"><img src="<?php print drupal_get_path("theme", "twenty"); ?>/images/pic03.jpg" alt="" /></a>
-                        <header>
-                            <h3>Hyperspace Travel</h3>
-                        </header>
-                        <p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
-                    </section>
-
-                </div>
-                <div class="6u">
-
-                    <section>
-                        <a href="#" class="image feature"><img src="<?php print drupal_get_path("theme", "twenty"); ?>/images/pic04.jpg" alt="" /></a>
-                        <header>
-                            <h3>And Another Train</h3>
-                        </header>
-                        <p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
-                    </section>
-
-                </div>
-            </div>
-
-            <footer class="major">
-                <ul class="buttons">
-                    <li><a href="#" class="button">See More</a></li>
-                </ul>
-            </footer>
-
-        </section>
-
     </article>
-
-    <!-- Call to Action Region -->
-    <?php if ($page['front_call_to_action_header_4'] || $page['front_call_to_action_footer_5']): ?>
-        <section id="cta">
-            <?php if ($page['front_call_to_action_header_4']): ?>
-                <header>
-                    <?php print render($page['front_call_to_action_header_4']); ?>
-                </header>
-            <?php endif; ?>
-            <?php if ($page['front_call_to_action_footer_5']): ?>
-                <footer>
-                    <?php print render($page['front_call_to_action_footer_5']); ?>
-                </footer>
-            <?php endif; ?>
-        </section>
-    <?php endif; ?>
 
     <!-- Footer -->
     <footer id="footer">
